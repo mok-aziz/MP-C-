2b9019d65502e7b0b23567901ba63499434d8018
Aziz Mokni <mohamedaziz.mokni@etudiant-enit.utm.tn>
2026-04-22 20:43:30 +0100

    Delete Buildings directory

--- Buildings/TownHall.cpp
+++
@@ -1,4 +0,0 @@
-#include "TownHall.h"
-// SizeX=5 SizeY=5 Cost=100/0 MaxInstances=1 HP=1000
-TownHall::TownHall(Position p)
-   : Building(p, 5, 5, "🏰", Resources(100, 0), 1, 1000) {}
--- Buildings/Wall.cpp
+++
@@ -1,4 +0,0 @@
-#include "Wall.h"
-// SizeX=1 SizeY=1 Cost=10 gold MaxInstances=200 HP=200
-Wall::Wall(Position pos)
-: Building(pos, 1, 1, "🧱", Resources(10,0), 200, 200) {}
